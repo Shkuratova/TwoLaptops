ee686542e68718baf3f0492e1f28ad78e5906cf5
Shkuratova <shkuratova.dv@dvfu.ru>
2023-10-18 20:00:39 +1000

    tl

---
+++ server.py
@@ -0,0 +1,41 @@
+import os
+import socket
+import sys
+
+from PyQt6.QtGui import QMovie
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QMainWindow
+
+
+class App(QMainWindow):
+    def __init__(self, gf):
+        super().__init__()
+        super().__init__()
+        self.wid = QWidget()
+        self.wid.setWindowTitle('Kitten')
+        self.setGeometry(100, 100, 500, 500)
+        self.lay = QVBoxLayout(self.wid)
+        self.lab = QLabel()
+        self.lay.addWidget(self.lab)
+        self.mov = QMovie(gf)
+        self.lab.setMovie(self.mov)
+        self.mov.start()
+        self.setCentralWidget(self.wid)
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('localhost', 12345))
+server.listen()
+data = ''
+flag = False
+while True:
+    user, adres = server.accept()
+    app = QApplication(sys.argv)
+    while True:
+        data = user.recv(1024).decode("utf-8")
+        win = App(data)
+        win.show()
+        break
+    sys.exit(app.exec())
+
+
+
